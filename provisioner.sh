0c175403c14317dd3ae2bd0f53278d69ba95e8de
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2015-11-02 09:44:44 +0100

    - Moved to mysql on production. - Excluded .vagrant folder from git.

--- provisioner.sh
+++ provisioner.sh
@@ -5,6 +5,8 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 
 sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
+sudo locale-gen UTF-8
+
 cd ~
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
