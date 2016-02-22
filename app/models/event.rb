8e025dceab9469db5d74f27aecfd05af814683ff
Matteo Gavagnin <m@macteo.it>
2016-02-22 16:28:34 +0100

    Integrated elastic search and events are saved to be analysed in realtime.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,29 @@
+require 'elasticsearch/persistence'
+
 class Event < ActiveRecord::Base
   include Rails.application.routes.url_helpers
+
   mount_uploader :attachment, AttachmentUploader
 
   after_create :write_to_file
   after_create :broadcast_event
+  after_create :save_on_elastic_search
 
   # To be able to use type as parameter
   self.inheritance_column = :_type_disabled
+
+  def save_on_elastic_search
+    repository = Elasticsearch::Persistence::Repository.new do
+      client Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'], log: true
+      index :events
+      type  :event
+      klass Event
+      settings number_of_shards: 1 do
+        mappings dynamic: 'true'
+      end
+    end
+    repository.save(self)
+  end
 
   def write_to_file
     dirPath = "log/#{self.app}"
@@ -42,15 +59,25 @@ class Event < ActiveRecord::Base
     return ""
   end
 
-  def socket_object
+  def socket_object(nested = true)
     hash = self.attributes
     hash["timestamp"] = self.timestamp.to_i
     if !payload.blank?
       begin
-        hash["payload"] = JSON.parse(payload, :quirks_mode => true)
+        if nested
+          hash["payload"] = JSON.parse(payload, :quirks_mode => true)
+        else
+          thePayload = JSON.parse(payload, :quirks_mode => true)
+          hash.merge!(Sparsify.sparse( thePayload, :separator => '-' ))
+        end
       rescue Exception => e
       end
     end
     return hash
   end
+
+  def to_hash
+    return socket_object(false)
+  end
+
 end
