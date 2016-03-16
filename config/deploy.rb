1254f29f12a3afcdd2909d385ef4338d22e3c958
Matteo Gavagnin <m@macteo.it>
2016-03-16 15:39:48 +0100

    Loading rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'apps_auth.yml'
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+  invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
   # invoke :'rvm:use[ruby-1.9.3-p125@default]'
